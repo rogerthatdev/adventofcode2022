bfbfbb197991d0837f421857295b1de3893d5716
Roger Martinez <roger.that.dev@gmail.com>
2023-11-29 07:16:02 -0800

    part B

--- 01/main.js
+++ 01/main.js
@@ -7,13 +7,16 @@ const readFile = util.promisify(fs.readFile);
 async function main() {
     try {
         return await readFile(filePath, 'utf8');
-    
+
     } catch (err) {
         console.error(err);
     }
 }
 main().then(data => {
     const elfArray = data.split('\n\n').map(elf => elf.split('\n'));
-    const calorieArray = elfArray.map(elf => elf.reduce((x,y) => Number(x)+Number(y)));
-    console.log(calorieArray.reduce((max, current)=> Math.max(max, current)), -Infinity);
+    const calorieArray = elfArray.map(elf => elf.reduce((x, y) => Number(x) + Number(y)));
+    // Most calories
+    console.log(calorieArray.reduce((max, current) => Math.max(max, current)), -Infinity);
+    // Total of top 3
+    console.log(calorieArray.sort((a, b) => b - a).slice(0, 3).reduce((x, y) => Number(x) + Number(y)));
 });
