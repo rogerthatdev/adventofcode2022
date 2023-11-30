deb7becbaa34e5e5734b2d97ceab905f7ee6888d
Roger Martinez <roger.that.dev@gmail.com>
2023-11-30 07:19:12 -0800

    day 02 pt2

--- 02/main.js
+++ 02/main.js
@@ -20,6 +20,12 @@ main()
                     return 2;
                 case 'Z':
                     return 3;
+                case 'rock':
+                    return 1;
+                case 'paper':
+                    return 2;
+                case 'scissors':
+                    return 3;
                 default:
                     return 0;
             }
@@ -50,7 +56,63 @@ main()
         const total = scoreArray.reduce((a, b) => a + b, 0);
 
         console.log(total) // 10310
-    })
+        
+        const convert = x => {
+            switch (x) {
+                case 'A':
+                    return 'rock';
+                case 'B':
+                    return 'paper';
+                case 'C':
+                    return 'scissors';
+                default:
+                    return null;
+            }
+        };
+
+        const outcomeMap = x => {
+            switch (x) {
+                case 'X':
+                    return 0;
+                case 'Y':
+                    return 3;
+                case 'Z':
+                    return 6;
+                default:
+                    return 0;
+            
+        }};
+
+        const whatToPlay = (opponent, outcome) => {
+            if (outcome == 'Y'){
+                return opponent;
+            }
+            else if (opponent == 'rock' && outcome == 'X') {
+                return 'scissors';
+            } else if (opponent == 'rock' && outcome == 'Z') {
+                return 'paper';
+            } else if (opponent == 'paper' && outcome == 'X') {
+                return 'rock';
+            } else if (opponent == 'paper' && outcome == 'Z') {
+                return 'scissors';
+            } else if (opponent == 'scissors' && outcome == 'X') {
+                return 'paper';
+            } else if (opponent == 'scissors' && outcome == 'Z') {
+                return 'rock';
+            }
+       }
+
+       const pt2TotalScore = (x, y) => {
+            return outcomeMap(y) + scoreMap(whatToPlay(convert(x), y))
+       };
+       
+       const pt2RoundArray = data.split('\n').map(x => x.split(' '))
+       console.log(pt2RoundArray)
+       const pt2ScoreArray = pt2RoundArray.map(x => pt2TotalScore(x[0], x[1]))
+       const pt2Total = pt2ScoreArray.reduce((a, b) => a + b, 0);
+
+       console.log(pt2Total) // 
+})
     .catch((err) => {
         console.error(err);
     });
